7c22be55cb8d862ed38f8beb8469ea97a614d06e
Jimmy <JimmyWangmy2016@gmail.com>
2017-11-15 22:30:37 +0800

    tse

---
+++ app/src/main/java/com/example/jimmy/myapplicationpreference/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.jimmy.myapplicationpreference;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        preferencesSettingTest();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static final String S_count ="setting_count";
+    public static final String S_user ="setting_user";
+    private void preferencesSettingTest(){
+
+
+        //get preference
+        PreferenceActivity pre;
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        int iCount = sp.getInt(S_count,0);
+        String suser = sp.getString(S_user,"user");
+        Log.d("Count:",""+iCount);
+        iCount++;
+        SharedPreferences.Editor se = sp.edit();
+        se.putInt(S_count, iCount);
+        se.putString(S_user,suser+iCount);
+        se.commit();
+    }
+}
